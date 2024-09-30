a8fa541df38be48d873754f8aa23d4f61ff618d8
Sticzx <mateusz2k7@wp.pl>
2024-09-30 11:47:48 +0200

    footer into main

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
+import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
