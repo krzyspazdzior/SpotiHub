73246c76661b19ef642ff895a64b644a59df77e7
krzyspazdzior <1p22pazdzior@gmail.com>
2024-10-01 09:29:37 +0200

    poczatek maina

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
 import { FooterComponent } from '../shared/footer/footer.component';
+import { BannerComponent } from "../shared/banner/banner/banner.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, BannerComponent],
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
