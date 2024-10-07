f7316f1ce01af98c6df27053f695352ca8871f68
krzyspazdzior <1p22pazdzior@gmail.com>
2024-10-07 11:57:56 +0200

    BIGASS COMMIT (aside, no footer, header updt, main updt)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
-import { FooterComponent } from '../shared/footer/footer.component';
 import { BannerComponent } from "../shared/banner/banner/banner.component";
+import { AsideComponent } from '../shared/aside/aside/aside.component';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
-  imports: [HeaderComponent, FooterComponent, BannerComponent],
+  imports: [HeaderComponent, BannerComponent, AsideComponent],
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
