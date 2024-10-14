e75945543b5104f6d63b2988386bd58eb6a81601
krzyspazdzior <1p22pazdzior@gmail.com>
2024-10-14 03:07:24 +0200

    Working form, playlists are displayed, popouts

--- src/app/library/library.component.ts
+++ src/app/library/library.component.ts
@@ -2,31 +2,143 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
 import { BannerComponent } from "../shared/banner/banner.component";
 import { AsideComponent } from '../shared/aside/aside.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { empty } from 'rxjs';
 
+
+
+interface Playlist{
+  id: number,
+  name: string;
+  songs: Song[];
+}
+class PlaylistInfo{
+  id: number;
+  name: string;
+  songs: Song[];
+  
+  constructor(id: number, name: string, songs: Song[]){
+    this.id = id;
+    this.name = name;
+    this.songs = songs;
+  }
+}
+let userPlaylists: Playlist[] = [];
+let allSongs: Song[] = []
+
+
+type releaseType = "album" | "single" | "EP";
+interface Song{
+  id: number,
+  title: string,
+  artist: string,
+  releaseType: releaseType,
+  album: number | null;
+}
+class SongInfo{
+  id: number;
+  title: string;
+  artist: string;
+  releaseType: releaseType;
+  album: number | null;
+  
+  
+  constructor(id: number, title: string, artist: string, releaseType: releaseType, album: number | null) {
+    this.id = id;
+    this.title = title;
+    this.artist = artist;
+    this.releaseType = releaseType;
+    this.album = album;
+  }
+}
 
 @Component({
   selector: 'app-library',
   standalone: true,
-  imports: [HeaderComponent, BannerComponent, AsideComponent],
+  imports: [HeaderComponent, BannerComponent, AsideComponent, ReactiveFormsModule],
   templateUrl: './library.component.html',
   styleUrls: ['./library.component.css', '../main-container.css']
 })
+
 export class LibraryComponent {
-  isShow = false;
+
+  // form display
+
+  isShowForm = false;
  
-  toggleDisplay(): void{
-    console.log("dziala");
-    this.isShow = !this.isShow;
+  toggleFormDisplay(): void{
+    this.isShowForm = !this.isShowForm;
   }
+
+  //form functionality
+
+  playlistForm = new FormGroup({
+    imageFile: new FormControl(null),
+    playlistName: new FormControl('New Playlist')
+  })
+
+
+  //creating playlist
+  
+  message: string = '';
+
+  createPlaylist(){
+
+    const emptyMessage = document.querySelectorAll('.empty-message');
+    if(emptyMessage){
+      emptyMessage.forEach((elem) => (
+        (elem as HTMLElement).style.display = "none"
+      ))
+    };
+    
+
+    let playlistName = this.playlistForm.value.playlistName ?? ''
+    
+    
+    const newPlaylistId = userPlaylists.length > 0 
+    ? Math.max(...userPlaylists.map(p => p.id)) + 1 : 1
+    userPlaylists.push(new PlaylistInfo(newPlaylistId, playlistName, []));
+
+    const playlistContainer = document.createElement('div');
+    playlistContainer.classList.add('library-item');
+    const playlistNumber = document.createElement('div');
+    playlistNumber.classList.add('item-number');
+    const playlistOptions = document.createElement('div');
+    playlistOptions.classList.add('item-options');
+  // Set inner HTML for the new playlist
+
+    playlistNumber.innerHTML = `${newPlaylistId}.`;
+    playlistOptions.innerHTML = `•••`
+
+  playlistContainer.innerHTML = `
+    <img src="/empty_playlist.png" alt="">
+    <div class="item-info">
+      <h3>${playlistName}</h3>
+      <p>Playlist • User</p>
+    </div>
+  `;
+
+  // Find the container where the playlists will be added
+  const libraryArea = document.querySelector('.library-area');
+  if (libraryArea) {
+    libraryArea.appendChild(playlistNumber);
+    libraryArea.appendChild(playlistContainer);
+    libraryArea.appendChild(playlistOptions);
+  }
+    const messageElement = document.getElementById("message");
+    this.message = `${playlistName} has been created in your library!`;
+    if (messageElement) {
+      messageElement.style.transition = "none";
+      messageElement.style.opacity = "1";
+      messageElement.innerText = this.message;
+      void messageElement.offsetWidth; 
+      messageElement.style.transition = "3.5s opacity";
+      setTimeout(() => {messageElement.style.opacity = "0";}, 1000)
+      setTimeout(() =>{messageElement.innerText = ""}, 4000)
+    }
+    this.playlistForm.reset();
+  }
+  
+  
+
 }
-// const form = document.querySelector('.form-container') as HTMLElement;
-// let formCheck = false;
-// function toggleForm(){
-//   if(formCheck == false){
-//     form.style.display = "flex";
-//     formCheck = true
-//   }else{
-//     form.style.display = "none";
-//     formCheck = false;
-//   }
-// }   
